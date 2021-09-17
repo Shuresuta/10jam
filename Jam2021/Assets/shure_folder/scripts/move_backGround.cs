cfcb1da7dcf466a6fc19f5e30c2b2074b6945d54
KuroShure <rekuiemu9@gmail.com>
2021-09-17 16:14:34 +0900

    サンプルシーンを作成その後8枚くらいの素材とか画像を追加スクリプトを一個追加

---
+++ Jam2021/Assets/shure_folder/scripts/move_backGround.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class move_backGround : MonoBehaviour
+{
+    [SerializeField] GameObject back_image;
+    
+    private int counter = 0;
+    private float move = -0.15f;
+    private int pos_X;
+    private int pos_y;
+    private int pos_z;
+    
+
+    void Update()
+    {
+        this.transform.position += new Vector3(move, 0, 0);
+        counter++;
+
+        if (counter == 13000)
+        {
+            counter = 0;
+            this.transform.position = new Vector3(1300,453,0);
+        }
+    }
+}
