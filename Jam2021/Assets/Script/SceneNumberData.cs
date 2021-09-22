a347a0e816c92fa08b1dc5223ce990b432007dfa
blue666225 <fwantazi1145@gmail.com>
2021-09-22 11:51:05 +0900

    セレクトボタンの移動と押したらシーン遷移するまで完成

---
+++ Jam2021/Assets/Script/SceneNumberData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneNumberData : MonoBehaviour
+{
+    public readonly static SceneNumberData numberData = new SceneNumberData();
+
+    public string referer = string.Empty;
+    
+}
