d6f69729ef9a4e24b444ac69dbf0dab7384d535f
KuroShure <rekuiemu9@gmail.com>
2021-09-23 10:03:30 +0900

    イラスト類を追加この後タイトルをいじる

--- Jam2021/Assets/shure_folder/scripts/move_backGround.cs
+++ Jam2021/Assets/shure_folder/scripts/move_backGround.cs
@@ -9,7 +9,7 @@ public class move_backGround : MonoBehaviour
     [SerializeField] GameObject back_image;
     
     private int counter = 0;
-    private float move = -0.15f;
+    private float move = -0.03f;
     private int pos_X;
     private int pos_y;
     private int pos_z;
@@ -23,7 +23,7 @@ public class move_backGround : MonoBehaviour
         if (counter == 530)
         {
             counter = 0;
-            this.transform.position = new Vector3(1300,318,0);
+            this.transform.position = new Vector3(0, 0, -900);
         }
     }
 }
